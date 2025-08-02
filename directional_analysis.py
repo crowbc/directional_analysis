31607fbc4ff2cebab94b9cfa61aa3f2532405d4c
crowbc <crowbc@hawaii.edu>
2025-08-01 17:29:39 -1000

    v 0.2.2: pdf Hotfix. See README.md for list of changes.

--- directional_analysis.py
+++ directional_analysis.py
@@ -20,19 +20,21 @@ import matplotlib.pyplot as plt
 #from matplotlib.ticker import MaxNLocator
 
 # Notes:
-# TODO: GUI and QOL improvements. Do money plot for 5, 50 and 150 mm segment sizes using 9, 5, and 3 matrix sizes, respectively
+# TODO: GUI and QOL improvements. Select statistical resolution plot (iterate fits, selectable, but default to 30) or covariance derived angular resolution plot.
 # TODO: For GUI, specify reference file (menu option) and data file (menu option), segment size (checkboxes, allowing all requiring at least one), pushbutton to calculate fits to data file, pushbutton to do resolution analysis
 # TODO: QOL improvement -- first time data is loaded for reference set, save it to a JSON. GUI option/prompt to load from JSON on start, or specify new reference set
 # segment size in mm
 # TODO: GUI/QOL improvement -- text box to specify number of iterations to perform for resolution analysis using statistical approach, radio button options to use either statistical or covariance matrix to determine resolution from fits
-
+# TODO: 100k event reference run? Discuss merits before committing, as it will most likely increase the time needed to do the fits.
 
 ################################################################
 ################################################################
 ################################################################
 class DirectionalAnalysis():
 	"""
-	DirectionalAnalysis class for use on processed RATPAC2 output files. This code reads the truth.txt and neutrons.txt files, bins the events in (normalized) directional matrices and performs Frobenius norm of the difference fits on these matrices to find the reference angle. The fits are performed multiple times for statistical uncertainty of the angle, which is then plotted as angular resolution as a function of number of events used in the fit.
+	DirectionalAnalysis class for use on processed RATPAC2 output files. This code reads the truth.txt and neutrons.txt files, bins the events in (normalized) directional matrices 
+	and performs Frobenius norm of the difference fits on these matrices to find the reference angle. The fits are performed multiple times for statistical uncertainty of the angle,
+	which is then plotted as angular resolution as a function of number of events used in the fit.
 	"""
 	################################################################
 	def __init__(self):
@@ -50,15 +52,24 @@ class DirectionalAnalysis():
 			plt.rcParams['mathtext.fontset'] = 'cm' # computer modern
 			
 		# data processing objects
-		self.pdg_codes = {"alpha": 1.00002004e+09, "triton": 1.00001003e+09, "neutron": 2112, "positron": -11}
-		self.num_ev_str = {10: "_10", 20: "_20", 50: "_50", 100: "_100", 200: "_200", 500: "_500", 1000: "_1k", 2000: "_2k", 5000: "_5k", 10000: "_10k"}
+		self.pdg_codes = {
+					"alpha": 1.00002004e+09, "triton": 1.00001003e+09, 
+					"neutron": 2112, "positron": -11
+		}
+		self.num_ev_str = {
+					10: "_10", 20: "_20", 50: "_50", 100: "_100", 200: "_200", 500: "_500", 
+					1000: "_1k", 2000: "_2k", 5000: "_5k", 10000: "_10k"
+		}
 		self.truth_file_ref = "/home/jack/RATPAC2/ratpac-setup/ratpac/output/mtv_directionality/res_10k_001wt_ref/truth.txt"
 		self.neutron_file_ref = "/home/jack/RATPAC2/ratpac-setup/ratpac/output/mtv_directionality/res_10k_001wt_ref/neutrons.txt"
 		self.truth_data_ref = self.readTruthData(self.truth_file_ref)
 		self.neutron_data_ref, self.truth_data_ref = self.readNeutrons(self.neutron_file_ref, self.truth_data_ref, self.Li6_filter)
-		# list objects for segment size and binning matrix size
-		self.seg_sz = [150, 50, 5]#[150]
-		self.grid_size = [3, 5, 17]#[3]
+		# vertices needed for binning and fits
+		self.ref_locs = self.neutron_data_ref.groupby("Row").last()# keep only captures
+		self.ref_truth_locs = self.truth_data_ref.groupby("Row").first()# keep only first entry of MC events
+		# list objects for segment size and binning matrix size --- note: use only 150 mm 3x3 matrix for testing and debugging
+		self.seg_sz = [150]#[150, 50, 5]
+		self.grid_size = [3]#[3, 5, 17]
 		self.out_file_path = "fits/"
 		self.iters = 5
 		return
@@ -104,14 +115,14 @@ class DirectionalAnalysis():
 		return positron_data
 	################################################################
 	# define binning function
-	def binEvents(self, filtered_data, truth_data, theta, seg_size, matrix_size, plot=False):
+	def binEvents(self, filtered_data, truth_data, theta, seg_size, matrix_size):#, plot=False):
 		if(self.debug):
 			print("DirectionalAnalysis class method: binEvents")
 		# Initializes matrix for segmentation.
 		seg = np.zeros((matrix_size, matrix_size), dtype=dict)
 		# Size of half of the total detector grid in mm.
 		half_size = seg_size * matrix_size / 2.0
-		# Initialize binning matrix for specified segment size and matrix size
+		# Initialize binning matrix for specified segment size and matrix size -- might help to make functions to initialize and re-zero matrices
 		for i in range(matrix_size):
 			for j in range(matrix_size):
 				xlow  = seg_size * j - half_size
@@ -175,7 +186,7 @@ class DirectionalAnalysis():
 		ref_str = pre_str + self.num_ev_str[num_events] + dop_str + run_type_str["r"]
 		# read truth.txt and neutrons.txt for fixed sets. Filter neutron events for captures on Li-6
 		fixed_set_truth = self.readTruthData(fixed_str + data_type_str["t"])
-		fixed_set_neutrons, fixed_set_truth = self.readNeutrons(fixed_str + data_type_str["n"], fixed_set_truth)
+		fixed_set_neutrons, fixed_set_truth = self.readNeutrons(fixed_str + data_type_str["n"], fixed_set_truth, self.Li6_filter)
 		# get neutron capture events and IBD vertices for fixed data set
 		locs = fixed_set_neutrons.groupby("Row").last()
 		truth_locs = fixed_set_truth.groupby("Row").first()# keep only the first entry. MC values don't change
@@ -185,24 +196,24 @@ class DirectionalAnalysis():
 		if(debug):
 			print(f"Number of binned captures: {num_caps}")
 		caps_fixed = caps_fixed/num_caps
-		ref_locs = self.neutron_data_ref.groupby("Row").last()# keep only captures
-		ref_truth_locs = self.truth_data_ref.groupby("Row").first()# keep only first entry of MC events
-		fnd_fit_str = f"Calculating FND fit #{it_num} for {fixed_str} and {ref_str} ..."
 		if (debug):
+			fnd_fit_str = f"Calculating FND fit #{it_num} for {fixed_str} and {ref_str} ..."
 			print(fnd_fit_str)
 		# Iterate over reference angles to minimize FND for |sin(th-th0)| fit
 		angles = []
 		norms = []
-		for th in range(-data_range,data_range):
+		# do binning of reference matrices --- TODO: needs status bar for binning progress (angle 1 to angle 2)
+		for th in range(-data_range,data_range):# TODO: do these reference binnings once and store. Recall binned matrices to do FND calculations for fits
 			angles.append(th)
-			caps_ref = self.binEvents(ref_locs, ref_truth_locs, th, seg_size, nbins)
-			caps_ref = caps_ref/np.sum(caps_ref)
+			caps_ref = self.binEvents(self.ref_locs, self.ref_truth_locs, th, seg_size, nbins)
+			num_ref = np.sum(caps_ref)
+			caps_ref = caps_ref/num_ref
 			fnd_sq = np.sum(np.square(caps_ref - caps_fixed))
 			fnd = np.sqrt(fnd_sq)
 			norms.append(fnd)
 		# set up the curve fit to norms or sum_sq
 		fit_range = 179
-		# use argmin of norms as initial guess for fit angle
+		# use argmin of norms as index for initial guess for fit angle and minimum
 		fnd_min = norms[np.argmin(norms)]
 		angle_guess = angles[np.argmin(norms)]
 		filename = f"segsz_{seg_size}mm_FND_fit{self.num_ev_str[num_events]}.txt"
@@ -254,20 +265,20 @@ class DirectionalAnalysis():
 		if (save_plots):
 			plt.savefig(output_path + f"plots/angle_{truth_angle}_deg_{seg_size}mm_segsz{self.num_ev_str[num_events]}_evts_fit_{it_num}.pdf", format="pdf")
 		plt.close()
-		return sin_popt[2]
+		return sin_popt[2], num_caps
 	################################################################
 	# moneyplot function iterates fits through various segment sizes, number of events in each "data" file, and number of iterations for fits
-	def resolutionPlot(self, n_iters, write_outputs=True, keep_plots=True, debug_msg=False):
+	def resolutionPlot(self, n_iters, f_name, write_outputs=True, keep_plots=True, show_plots=False, debug_msg=False):
 		# matrix size for n by n binning matrix -- events are binned by counting the number of segments between prompt and delayed vertices
 		# TODO: find proper binning size for 5 mm segments
 		seg_style = {150: "b--", 50: "b-.", 5: "b:"}#{150: "b--"}
 		fit_res = {150: [], 50: [], 5: []}#{150: []}
 		n_evts = [10, 20, 50, 100, 200, 500, 1000, 2000, 5000, 10000]
 		dir_str = {
-			10: "moneyplot_10_evt_fits/", 20: "moneyplot_20_evt_fits/", 50: "moneyplot_50_evt_fits/", 
-			100: "moneyplot_100_evt_fits/", 200: "moneyplot_200_evt_fits/", 500: "moneyplot_500_evt_fits/", 
-			1000: "moneyplot_1k_evt_fits/", 2000: "moneyplot_2k_evt_fits/", 5000: "moneyplot_5k_evt_fits/", 
-			10000: "moneyplot_10k_evt_fits/"
+				10: "moneyplot_10_evt_fits/", 20: "moneyplot_20_evt_fits/", 50: "moneyplot_50_evt_fits/", 
+				100: "moneyplot_100_evt_fits/", 200: "moneyplot_200_evt_fits/", 500: "moneyplot_500_evt_fits/", 
+				1000: "moneyplot_1k_evt_fits/", 2000: "moneyplot_2k_evt_fits/", 5000: "moneyplot_5k_evt_fits/", 
+				10000: "moneyplot_10k_evt_fits/"
 		}
 		fig, ax1 = plt.subplots(figsize=(6, 4))
 		ax1.set_xlabel("Number of events")
@@ -276,21 +287,24 @@ class DirectionalAnalysis():
 		ax1.set_yscale("log")
 		for k, sz in enumerate(self.seg_sz):
 			fit_angle = {10: [], 20: [], 50: [], 100: [], 200: [], 500: [], 1000: [], 2000: [], 5000: [], 10000: []}
-			#num_fits = {10: [], 20: [], 50: [], 100: [], 200: [], 500: [], 1000: [], 2000: [], 5000: [], 10000: []}
+			num_fits = {10: [], 20: [], 50: [], 100: [], 200: [], 500: [], 1000: [], 2000: [], 5000: [], 10000: []}
 			fit_means = {10: 0, 20: 0, 50: 0, 100: 0, 200: 0, 500: 0, 1000: 0, 2000: 0, 5000: 0, 10000: 0}
 			fit_stds = {10: 0, 20: 0, 50: 0, 100: 0, 200: 0, 500: 0, 1000: 0, 2000: 0, 5000: 0, 10000: 0}
 			for j, num in enumerate(n_evts):
 				if (debug_msg):
 					print(f"Calculating fits and angular resolution for {num} events in {sz}mm segments with {iters} iterations...\n")
-				out_file_str = f"seg_size_{sz}_resolution_{self.num_ev_str[num]}_evts.txt"
+				out_file_str = f"seg_size_{sz}mm_resolution{self.num_ev_str[num]}_evts.txt"
 				for i in range(n_iters):
-					angle = self.angleFit(self.grid_size[k], sz, num, i, write_outputs, keep_plots, self.out_file_path + dir_str[num], False, False)
+					angle, n_caps = self.angleFit(self.grid_size[k], sz, num, i, write_outputs, keep_plots, self.out_file_path + dir_str[num], False, False)
 					fit_angle[num].append(angle)
+					num_fits[num].append(n_caps)
+				n_recons = np.mean(num_fits[num])
 				fit_means[num] = np.mean(fit_angle[num])
 				fit_stds[num] = np.std(fit_angle[num])
 				fit_res[sz].append(fit_stds[num])
 				out_str = "Fit value and angular resolution for "
 				out_str += f"{sz}mm after {self.iters} iterations on {num} events (deg.)\nfit: {fit_means[num]} \tres: {fit_stds[num]}\n"
+				out_str += f"Average number of reconstructions on {num} events: {n_recons} \n"
 				if (write_outputs):
 					with open(self.out_file_path + dir_str[num] + out_file_str, "w") as f:
 						print(out_str, file=f)
@@ -309,9 +323,11 @@ class DirectionalAnalysis():
 		# show me the money!!!
 		ax1.legend(loc="upper right", framealpha=1.0)
 		fig.tight_layout()
-		plt.show()
+		#plt.show()
 		if (keep_plots):
-			plt.savefig(self.out_file_path + f"moneyplot_{self.iters}iters.pdf", format="pdf")
+			plt.savefig(self.out_file_path + f_name, format="pdf")
+		if (show_plots):
+			plt.show()
 		plt.close()
 		return
 ################################################################
@@ -321,7 +337,8 @@ if __name__ == "__main__":
     """
 da = DirectionalAnalysis()
 itnums = 5
-da.resolutionPlot(itnums)
+plot_name = f"ang_res_plot_{itnums}iters.pdf"
+da.resolutionPlot(itnums, plot_name)
 sys.exit()
 # dumping ground for unused code
 
